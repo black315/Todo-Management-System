9b652a10375b90b20c36e17daf0dcd2798a8448e
black315 <ryo.0301.1999@gmail.com>
2019-08-17 08:00:12 +0000

    implement service

--- src/main/java/com/rakuten/internship/service/TodoService.java
+++ src/main/java/com/rakuten/internship/service/TodoService.java
@@ -3,6 +3,9 @@ package com.rakuten.internship.service;
 import java.util.List;
 
 import com.rakuten.internship.entity.Todo;
+import com.rakuten.internship.repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,15 +16,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TodoService {
 
-    // TODO 必要なメンバーを宣言してください。
+    @Autowired
+    private TodoRepository todoRepository;
 
+    /**
+     * Todoを全件取得します
+     * @return Todo一覧
+     */
     public List<Todo> findTodos(){
-        // TODO 必要なコードを作成してください。
-        return null;
+        return todoRepository.findAll();
     }
 
+    /**
+     * Todoを保存します
+     * @param todo Todo
+     * @return 保存したTodo
+     */
     public Todo save(Todo todo) {
-        // TODO 必要なコードを作成してください。
-        return null;
+        return todoRepository.save(todo);
     }
 }
