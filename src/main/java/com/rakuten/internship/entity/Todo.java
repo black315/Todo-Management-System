770a825b079c7dc219b5fb846c377e107e5fb863
black315 <ryo.0301.1999@gmail.com>
2019-08-17 07:36:48 +0000

    create entity

--- src/main/java/com/rakuten/internship/entity/Todo.java
+++ src/main/java/com/rakuten/internship/entity/Todo.java
@@ -1,9 +1,73 @@
 package com.rakuten.internship.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * このクラスは、タスク管理の対象となるTodoのエンティティーです。
  * エンティティーとして使えるように、コードを記入してください。
  */
+@Entity
 public class Todo {
-    // TODO 必要なコードを記入してください。
+    
+    /** ID */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+   
+    /** タイトル */
+    private String title;
+  
+    /** 説明 */
+    private String description;
+    
+    /**
+     * IDを取得します
+     * @return ID
+     */
+    public Long getId() {
+        return id;
+    }
+    
+    /**
+     * IDをセットします
+     * @param id ID
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    /**
+     * タイトルを取得します
+     * @return タイトル
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * タイトルをセットします
+     * @param title タイトル
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    /**
+     * 説明を取得します
+     * @return 説明
+     */
+    public String getDescription() {
+        return description;
+    }
+    
+    /**
+     * 説明をセットします説明をセット
+     * @param description 説明
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
